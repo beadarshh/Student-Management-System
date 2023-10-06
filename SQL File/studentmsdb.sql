52596fa61f4f61bde47839b5b6e5239647a86b1b
Krishan Murari <144571603+SShayar@users.noreply.github.com>
2023-10-06 14:59:46 -0700

    Add files via upload

--- SQL File/studentmsdb.sql
+++ SQL File/studentmsdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2022 at 08:41 PM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.2.19
+-- Generation Time: Sep 29, 2023 at 01:33 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sturecdb`
+-- Database: `studentmsdb`
 --
 
 -- --------------------------------------------------------
@@ -36,14 +35,14 @@ CREATE TABLE `tbladmin` (
   `Email` varchar(200) DEFAULT NULL,
   `Password` varchar(200) DEFAULT NULL,
   `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tbladmin`
 --
 
 INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
-(1, 'Admin', 'admin', 8979555558, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2019-10-11 04:36:52');
+(1, 'Krishan Murari ', 'Krishan', 9872643540, 'krishanmurari.gjimt@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2023-09-27 04:36:52');
 
 -- --------------------------------------------------------
 
@@ -56,22 +55,20 @@ CREATE TABLE `tblclass` (
   `ClassName` varchar(50) DEFAULT NULL,
   `Section` varchar(20) DEFAULT NULL,
   `CreationDate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblclass`
 --
 
 INSERT INTO `tblclass` (`ID`, `ClassName`, `Section`, `CreationDate`) VALUES
-(1, '10', 'A', '2022-01-13 10:42:14'),
-(2, '10', 'B', '2022-01-13 10:42:35'),
-(3, '10', 'C', '2022-01-13 10:42:41'),
-(4, '11', 'A', '2022-01-13 10:42:47'),
-(5, '11', 'B', '2022-01-13 10:42:52'),
-(6, '11', 'C', '2022-01-13 10:42:57'),
-(7, '11', 'D', '2022-01-13 10:43:04'),
-(8, '12', 'A', '2022-01-13 10:43:10'),
-(9, '12', 'C', '2022-01-13 10:43:15');
+(12, 'B.Com Hons.', 'A', '2023-09-26 19:18:33'),
+(13, 'B.Com Hons.', 'B', '2023-09-26 19:18:42'),
+(14, 'B.B.A', 'A', '2023-09-26 19:19:18'),
+(15, 'B.B.A', 'B', '2023-09-26 19:19:26'),
+(16, 'M.B.A', 'A', '2023-09-26 19:20:14'),
+(17, 'B.C.A.', 'A', '2023-09-26 19:20:32'),
+(18, 'M.C.A.', 'B', '2023-09-26 19:20:47');
 
 -- --------------------------------------------------------
 
@@ -85,7 +82,7 @@ CREATE TABLE `tblnotice` (
   `ClassId` int(10) DEFAULT NULL,
   `NoticeMsg` mediumtext DEFAULT NULL,
   `CreationDate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblnotice`
@@ -95,7 +92,8 @@ INSERT INTO `tblnotice` (`ID`, `NoticeTitle`, `ClassId`, `NoticeMsg`, `CreationD
 (2, 'Marks of Unit Test.', 3, 'Meet your class teacher for seeing copies of unit test', '2022-01-19 06:35:58'),
 (3, 'Marks of Unit Test.', 2, 'Meet your class teacher for seeing copies of unit test', '2022-01-19 06:35:58'),
 (4, 'Test', 3, 'This is for testing.', '2022-02-02 18:17:03'),
-(5, 'Test Notice', 8, 'This is for Testing.', '2022-02-02 19:03:43');
+(5, 'Test Notice', 8, 'This is for Testing.', '2022-02-02 19:03:43'),
+(6, 'MST -(1ST) ', 17, 'Class MST will be started from 4 October', '2023-09-26 19:27:12');
 
 -- --------------------------------------------------------
 
@@ -111,15 +109,15 @@ CREATE TABLE `tblpage` (
   `Email` varchar(200) DEFAULT NULL,
   `MobileNumber` bigint(10) DEFAULT NULL,
   `UpdationDate` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblpage`
 --
 
 INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
-(1, 'aboutus', 'About Us', '<div style=\"text-align: start;\"><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, ????, ??, SimSun, STXihei, ????, serif\"><span style=\"font-size: 26px;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></font><br></div>', NULL, NULL, NULL),
-(2, 'contactus', 'Contact Us', '890,Sector 62, Gyan Sarovar, GAIL Noida(Delhi/NCR)', 'infodata@gmail.com', 7896541236, NULL);
+(1, 'aboutus', 'ABOUT US', '<div style=\"text-align: start;\"><font color=\"#111111\" face=\"nimbus-sans\"><b>Gian Jyoti Institute of Management and Technology, Mohali offers undergraduate, postgraduate and doctoral programmes. GJIMT Mohali courses are offered in management and business administration, arts, humanities and social sciences, computer application and IT and commerce streams. GJIMT Mohali UG courses are B.Com Hons., BCA and BBA. GJIMT Mohali PG courses include MCA and MBA. GJIMT Mohali PhD courses are available in multiple specialisations. GJIMT fees and eligibility differ as per courses at different levels.&nbsp;</b></font></div>', NULL, NULL, NULL),
+(2, 'contactus', 'CONTACT US', 'Phase-2, Mohali, Sector-54, Chandigarh, Mohali Pin Code: 160055', 'gjimt@gjimt.com', 9914433199, NULL);
 
 -- --------------------------------------------------------
 
@@ -132,7 +130,7 @@ CREATE TABLE `tblpublicnotice` (
   `NoticeTitle` varchar(200) DEFAULT NULL,
   `NoticeMessage` mediumtext DEFAULT NULL,
   `CreationDate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblpublicnotice`
@@ -140,7 +138,8 @@ CREATE TABLE `tblpublicnotice` (
 
 INSERT INTO `tblpublicnotice` (`ID`, `NoticeTitle`, `NoticeMessage`, `CreationDate`) VALUES
 (1, 'School will re-open', 'Consult your class teacher.', '2022-01-20 09:11:57'),
-(2, 'Test Public Notice', 'This is for Testing\r\n', '2022-02-02 19:04:10');
+(2, 'Test Public Notice', 'This is for Testing\r\n', '2022-02-02 19:04:10'),
+(3, 'Pratibha', 'Student Tight Your Seat belts and be ready to rock in Pratibha ', '2023-09-26 19:28:26');
 
 -- --------------------------------------------------------
 
@@ -165,7 +164,7 @@ CREATE TABLE `tblstudent` (
   `Password` varchar(200) DEFAULT NULL,
   `Image` varchar(200) DEFAULT NULL,
   `DateofAdmission` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblstudent`
@@ -176,7 +175,9 @@ INSERT INTO `tblstudent` (`ID`, `StudentName`, `StudentEmail`, `StudentClass`, `
 (2, 'Kishore Sharma', 'kishore@gmail.com', '3', 'Male', '2019-01-05', '10A12345', 'Janak Sharma', 'Indra Devi', 7879879879, 7987979879, 'kjhkhjkhdkshfiludzshfiu\r\nfjedh\r\nk;jk', 'kishore2019', '202cb962ac59075b964b07152d234b70', '5bede9f47102611b4df6241c718af7fc1642314213.jpg', '2022-01-16 06:23:33'),
 (3, 'Anshul', 'anshul@gmali.com', '2', 'Female', '1986-01-05', 'uii-990', 'Kailesg', 'jakinnm', 4646546546, 6546598798, 'jlkjkljoiujiouoil', 'anshul1986', '202cb962ac59075b964b07152d234b70', '4f0691cfe48c8f74fe413c7b92391ff41642605892.jpg', '2022-01-19 15:24:52'),
 (4, 'John Doe', 'john@gmail.com', '1', 'Female', '2002-02-10', '10806121', 'Anuj Kumar', 'Garima Singh', 1234698741, 1234567890, 'New Delhi', 'john12', 'f925916e2754e5e03f75dd58a5733251', 'ebcd036a0db50db993ae98ce380f64191643825985.png', '2022-02-02 18:19:45'),
-(5, 'Anuj kumar Singh', 'akkr@gmail.com', '8', 'Male', '2001-05-30', '1080623', 'Bijendra Singh', 'Kamlesh Devi', 1472589630, 1236987450, 'New Delhi', 'anujk3', 'f925916e2754e5e03f75dd58a5733251', '2f413c4becfa2db4bc4fc2ccead84f651643828242.png', '2022-02-02 18:57:22');
+(5, 'Anuj kumar Singh', 'akkr@gmail.com', '8', 'Male', '2001-05-30', '1080623', 'Bijendra Singh', 'Kamlesh Devi', 1472589630, 1236987450, 'New Delhi', 'anujk3', 'f925916e2754e5e03f75dd58a5733251', '2f413c4becfa2db4bc4fc2ccead84f651643828242.png', '2022-02-02 18:57:22'),
+(6, 'Amit Singh Bhandari jr', 'Amit@gmail.com', '17', 'Male', '2002-02-28', '2111745', 'Amit Singh Bhandari', 'Amit Singh Bhandari', 9872643540, 9115880673, 'house no. 8 Sector-122\r\nBehlolpur', 'Amit@gmail.com', '838e372ff139f4b9205b08725a945ed6', 'ce9992984ecf0131013ec5fe7dca63a81695756245.jpg', '2023-09-26 19:24:05'),
+(7, 'Amit Singh Bhandari', 'Amit@gmail.com', '15', 'Male', '2002-12-12', '2111746', 'Amit Singh Bhandari', 'Amit Singh Bhandari', 9872643540, 9115880673, 'house no. 8 Sector-122\r\nBehlolpur', 'Amit2@gmail.com', '838e372ff139f4b9205b08725a945ed6', 'aadd924fe412d4a549eb1db1653806461695756315.jpg', '2023-09-26 19:25:15');
 
 --
 -- Indexes for dumped tables
@@ -232,13 +233,13 @@ ALTER TABLE `tbladmin`
 -- AUTO_INCREMENT for table `tblclass`
 --
 ALTER TABLE `tblclass`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tblnotice`
 --
 ALTER TABLE `tblnotice`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tblpage`
@@ -250,13 +251,13 @@ ALTER TABLE `tblpage`
 -- AUTO_INCREMENT for table `tblpublicnotice`
 --
 ALTER TABLE `tblpublicnotice`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tblstudent`
 --
 ALTER TABLE `tblstudent`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
